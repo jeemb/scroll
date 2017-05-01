0d9f1e28a44c1bd1e2963c7618488e5c9f6e3e84
jb <jenniferbeem@gmail.com>
2017-05-01 12:42:33 -0700

    add scrollspy functionality, initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    routing
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]

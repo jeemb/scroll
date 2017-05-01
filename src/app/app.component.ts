d2efa6ed0fb08e5456d001a865baaa60d96c3d26
Dylan Stackhouse and Jennifer Beem <dylan@>
2017-05-01 14:35:56 -0700

    add databinding to nav bar buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  location = window.pageYOffset;
+  section1 = null;
+  section2 = null;
+  section3 = null;
+  section4 = null;
+  ngOnInit() {
+    this.section1 = document.getElementById("section1");
+    this.section2 = document.getElementById("section2");
+    this.section3 = document.getElementById("section3");
+    this.section4 = document.getElementById("section4");
+
+    document.addEventListener("scroll",(e)=>{
+        this.location = window.pageYOffset;
+        console.log(this.location);
+    })
+  }
+
+  isActive(key) {
+
+      if (this.location+40 > this[key].offsetTop && this.location+40 < this[key].offsetTop + this[key].offsetHeight ){
+          return "active";
+      } else {
+          return "notActive";
+      }
+  }
 }
